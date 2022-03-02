8790dcbdadd0bbea30710758650cb3420d3a5a6c
Bursade <lautajfp@gmail.com>
2022-03-01 22:30:35 -0300

    walking @

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,20 @@
-
-#include <SDL.h>
-#include <libtcod.h>
-
-#include <cstdlib>
-
-int main(int argc, char** argv) {
-  auto params = TCOD_ContextParams{};
-  params.tcod_version = TCOD_COMPILEDVERSION;
-  params.argc = argc;
-  params.argv = argv;
-  params.vsync = 1;
-  params.sdl_window_flags = SDL_WINDOW_RESIZABLE;
-  params.window_title = "Rogue Engine";
-
-  auto console = tcod::Console{80, 25};
-  params.console = console.get();
-
-  auto context = tcod::new_context(params);
-
-  // Game loop.
-  while (true) {
-    // Rendering.
-    console.clear();
-    tcod::print(console, {0, 0}, "Hello World", TCOD_white, std::nullopt);
-    context->present(console);
-
-    // Handle input.
-    SDL_Event event;
-    SDL_WaitEvent(nullptr);
-    while (SDL_PollEvent(&event)) {
-      switch (event.type) {
-        case SDL_QUIT:
-          std::exit(EXIT_SUCCESS);
-          break;
-      }
-    }
-  }
-  return 0;
+#include "libtcod.hpp"
+int main() {
+   int playerx=40,playery=25;
+   TCODConsole::initRoot(80,50,"libtcod C++ tutorial",false);
+   while ( !TCODConsole::isWindowClosed() ) {
+       TCOD_key_t key;
+       TCODSystem::checkForEvent(TCOD_EVENT_KEY_PRESS,&key,NULL);
+       switch(key.vk) {
+           case TCODK_UP : playery--; break;
+           case TCODK_DOWN : playery++; break;
+           case TCODK_LEFT : playerx--; break;
+           case TCODK_RIGHT : playerx++; break;
+           default:break;
+       }
+       TCODConsole::root->clear();
+       TCODConsole::root->putChar(playerx,playery,'@');
+       TCODConsole::flush();
+   }
+   return 0;
 }
